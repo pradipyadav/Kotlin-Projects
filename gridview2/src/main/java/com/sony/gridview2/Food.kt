49b501d20fe4590b203a5c721f06651390657fa0
sony <yadav.pradip1995@gmail.com>
2020-04-18 11:13:31 +0530

    App:Grid View

---
+++ gridview2/src/main/java/com/sony/gridview2/Food.kt
@@ -0,0 +1,14 @@
+package com.sony.gridview2
+
+class Food {
+
+    var name:String?=null
+    var des:String?=null
+    var img:Int?=null
+
+    constructor(name: String?, des: String?, img: Int?) {
+        this.name = name
+        this.des = des
+        this.img = img
+    }
+}
---
+++ gridview2/src/main/java/com/sony/gridview2/FoodDetails.kt
@@ -0,0 +1,19 @@
+package com.sony.gridview2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_food_details.*
+import kotlinx.android.synthetic.main.food_ticket.*
+import kotlinx.android.synthetic.main.food_ticket.ivFoodImage
+import kotlinx.android.synthetic.main.food_ticket.tvName
+
+class FoodDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_details)
+
+        val bundle=intent.extras
+        ivFoodImage.setImageResource(bundle!!.getInt("image"))
+        tvName.text = bundle.getString("name")
+        tvDetails.text = bundle.getString("des")    }
+}
