4177bf6f08f1209413d6e7fafec45589cbfcae15
sony <yadav.pradip1995@gmail.com>
2020-05-24 19:49:31 +0530

    App:RecyclerViewDemo

---
+++ androidrecyclerview/src/main/java/com/sony/androidrecyclerview/MoviesAPI.kt
@@ -0,0 +1,24 @@
+package com.sony.androidrecyclerview
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+const val BASE_URL = "https://api.simplifiedcoding.in/course-apis/recyclerview/"
+
+interface MoviesApi {
+
+    @GET("movies")
+    fun getMovies(): Call<List<Movie>>
+
+    companion object {
+        operator fun invoke(): MoviesApi {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(MoviesApi::class.java)
+        }
+    }
+}
