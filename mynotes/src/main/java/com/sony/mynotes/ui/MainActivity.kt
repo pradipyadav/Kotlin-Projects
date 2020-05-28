e40257499da9ebd8324de2e5ad896cd71fbec110
sony <yadav.pradip1995@gmail.com>
2020-05-28 20:51:08 +0530

    Simple Notepad App:Android Room DB UI

---
+++ mynotes/src/main/java/com/sony/mynotes/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sony.mynotes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.sony.mynotes.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController=Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.fragment),
+        null)
+    }
+}
